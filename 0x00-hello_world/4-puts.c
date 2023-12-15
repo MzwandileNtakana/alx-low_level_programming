093b338bba4ba1ac71fdb44d54ece07eb27bf9c6
Mzwandile Milton <mzwandilentakana@gmail.com>
2023-12-15 05:04:12 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints with puts functions
- * Return : 0 (Success)
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message using puts
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
+
