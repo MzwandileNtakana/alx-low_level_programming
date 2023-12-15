bd7044de67eabf1f9457c93f0c2a5640890eb3f2
Mzwandile Milton <mzwandilentakana@gmail.com>
2023-12-15 05:10:41 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a piece of art with proper grammar
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("This is a piece of art,\n");
+	return (0);
+}
+
