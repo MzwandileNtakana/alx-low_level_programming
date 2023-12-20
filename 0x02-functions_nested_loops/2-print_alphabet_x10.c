a62267602fd746092e949b15d1bec42b3638c89b
Mzwandile Milton <mzwandilentakana@gmail.com>
2023-12-20 00:54:50 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print alphabet - main function
+ * Return: always return 0
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
